4227ad60da033f22c0680eb69d807b053a0b302e
LeighEarl <pidoleighearl00@gmail.com>
2022-09-21 14:55:25 +0800

    PHP567

---
+++ PIDO_PHPexercise5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Exercise5</title>
+</head>
+<body>
+    
+<?php   
+ $around = "around"; 
+ echo 'What goes ' .$around. ' comes ' .$around. '.'; 
+ ?>
+
+</body>
+</html>
---
+++ PIDO_PHPexercise6.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Exercise6</title>
+</head>
+<body>
+ <?php
+ for ($x = 1; $x <= 12; $x++){
+   $list = $x * $x;
+   echo "$x * $x = $list <br/>";
+ }
+ ?>
+</body>
+</html>
