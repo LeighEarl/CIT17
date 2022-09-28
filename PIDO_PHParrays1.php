6bcff8121d593f05ee3d5f7f9c87201af07ae37f
LeighEarl <pidoleighearl00@gmail.com>
2022-09-29 00:01:04 +0800

    arraysPHP12

---
+++ PIDO_PHParrays1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PIDOPHPExerciseArrays1</title>
+</head>
+<body>
+
+<h1>In the middle of September</h1>
+ 
+ <?php
+ $weather=array("rain", "sunshine","clouds", "hail", "sleet","snow", "wind");
+ echo "We've seen all kinds of weather this month. At the beginning of the ";
+ echo "month,we had $weather[5] and $weather[6]. Then came $weather[1] with a few";
+ echo " $weather[2] and some $weather[0]. At least we didn't get any $weather[3] or $weather[4].";
+ ?>
+ 
+</body>
+</html>
