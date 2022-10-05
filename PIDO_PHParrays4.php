a06b63773a4bf31cccc38800ffc98762609ed867
LeighEarl <64972220+LeighEarl@users.noreply.github.com>
2022-10-05 23:42:30 +0800

    Update PIDO_PHParrays4.php

--- PIDO_PHParrays4.php
+++ PIDO_PHParrays4.php
@@ -6,41 +6,39 @@
 <?php
 
 $a = array (25,12,43);
-
 $fr = array_fill(0, count($a), 0);
 $check = -1;
+
+echo("Input 3 elements in the array: <br> ");
 for($i = 0; $i < count($a); $i++)
 {
-$count = 1;
-for($j = $i+1; $j < count($a); $j++)
-{
-if($a[$i] == $a[$j])
-{
-$count++;
+	$key=$a[$i];
+	$count = 1;
+		for($j = $i+1; $j < count($a); $j++)
+		{
+			if($a[$i] == $a[$j])
+			{
+				$count++;	
+				$fr[$j] = $check;
+			}
+		}
+		if($fr[$i] != $check)
+		$fr[$i] =$count;
+        echo "element - [".$i."]"." : ".$key. "<br>";
+}
 
-$fr[$j] = $check;
-}
-}
-if($fr[$i] != $check)
-$fr[$i] =$count;
-}
-echo("Input 3 elements in the array: <br> ");
-foreach( $a as $b )
-{
-echo "element - [".$i."]"." : ".$b. "<br>";
-}
 
 echo ("<br>The frequency of all elements of an array : <br>" );
 for( $i = 0; $i < count($fr); $i++)
 {
-if($fr[$i] != $check)
-{
-echo( $a[$i] ." occurs " );
-echo( $fr[$i]." times.");
-echo(" <br>");
+	if($fr[$i] != $check)
+	{
+	echo( $a[$i] ." occurs " );
+	echo( $fr[$i]." times.");
+	echo("<br>");
+	}
 }
-}
-return 0;
+
 ?>
 </body>
 </html>
